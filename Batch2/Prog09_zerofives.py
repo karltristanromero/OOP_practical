5816786921d85ebcb936c59343f9fd86740d5890
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-07 17:25:55 +0800

    perf: divisibility of 5 is enough

--- Batch2/Prog09_zerofives.py
+++ Batch2/Prog09_zerofives.py
@@ -1,5 +1,5 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
-for i in range(100):
-    if i % 10 != 0 and i % 5 != 0:
+for i in range(101):
+    if i % 5 != 0:
         print(i, end=" ")
