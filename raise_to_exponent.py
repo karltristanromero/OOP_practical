127a5617e18f1e9a1e1e38f3e61ca2ce07cfccec
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:19:06 +0800

    style: change variable names of inputs for better description

--- raise_to_exponent.py
+++ raise_to_exponent.py
@@ -1,6 +1,6 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
-x = float(input("First number: "))
-y = float(input("Second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
-print(f"{x} raised to {y} is {x ** y}")
+print(f"{first_number} raised to {second_number} is {first_number ** second_number}")
