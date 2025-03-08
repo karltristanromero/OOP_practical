ed0ab9233b290564f0041febadb8e525efaf1d55
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 21:49:40 +0800

    style: change variable name to be more descriptive

--- bigger_number.py
+++ bigger_number.py
@@ -1,5 +1,6 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
-x = float(input("First number: "))
-y = float(input("Second number: "))
-print(f"{x if x > y else y} is larger.")
+first_number = float(input("First number: "))
+second_number = float(input("Second number: "))
+
+print(f"{first_number if first_number > second_number else second_number} is larger.")
