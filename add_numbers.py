99c7be43577f6a446941b6ffab41db126fe792d8
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 22:06:07 +0800

    style: variable name change of the inputs for better description

--- add_numbers.py
+++ add_numbers.py
@@ -1,6 +1,6 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
-x = float(input("First number: "))
-y = float(input("Second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
-print(f"The sum of the two numbers is: {x + y}")
+print(f"The sum of the two numbers is: {first_number + second_number}")
