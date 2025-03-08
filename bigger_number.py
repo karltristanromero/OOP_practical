8538c9dbb50dda1eeab7f989fc84a0954c615b3c
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-08 21:54:36 +0800

    refactor: changed list comprehension into if-else statements

--- bigger_number.py
+++ bigger_number.py
@@ -1,6 +1,9 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
-first_number = float(input("First number: "))
-second_number = float(input("Second number: "))
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
 
-print(f"{first_number if first_number > second_number else second_number} is larger.")
+if first_number > second_number:
+    print(f"The number {first_number} is bigger than {second_number}.")
+elif first_number < second_number:
+    print(f"The number {second_number} is bigger than {first_number}.")
